542fdbbe8c1f5693505de68370332cf256c7ed8f
Julia lopes <jlr.nwb@gmail.com>
2023-02-15 21:23:06 -0300

    API Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=` + process.env.API_KEY;
+
+  const searchLocation = (event: any) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
 
   return (
     <div className="App">
